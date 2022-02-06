98caab378b793aa3212f7af4d257665bfa1b18b5
Josh Glazer <joshglazer@gmail.com>
2022-02-06 17:26:11 -0500

    refactor(sonar-smells): cleaned up sonar smells
    added better typing for the PersonDetails component

---
+++ api/tmdbApi.ts
@@ -0,0 +1,11 @@
+import { TmdbApi } from "tmdb-typescript-api";
+
+const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
+const TMDB_BASE_URL = "https://image.tmdb.org/t/p/w300/";
+
+function getTmdbApiClient(): TmdbApi {
+  return new TmdbApi(TMDB_API_KEY || "");
+}
+
+export { getTmdbApiClient, TMDB_BASE_URL };
--- components/PersonSearchField.tsx
+++ components/PersonSearchField.tsx
@@ -1,30 +1,21 @@
 import { debounce } from "lodash";
-import React, { forwardRef, MouseEvent, useRef, useState } from "react";
-import {
-  UseFormRegister,
-  FieldValues,
-  Control,
-  UseFormSetValue,
-} from "react-hook-form";
+import React, { MouseEvent, useRef, useState } from "react";
+import { FieldValues, UseFormSetValue } from "react-hook-form";
 import {
   Form,
-  Ref,
   Search,
   SearchProps,
   SearchResultData,
   StrictSearchResultProps,
 } from "semantic-ui-react";
-import { Movie, Person, TmdbApi, TvShow } from "tmdb-typescript-api";
+import { Movie, Person, TvShow } from "tmdb-typescript-api";
+import { getTmdbApiClient, TMDB_BASE_URL } from "../api/tmdbApi";
 
 interface PersonSearchFieldProps {
   name: string;
   label: string;
   setValue: UseFormSetValue<FieldValues>;
 }
-
-const tmdbApiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-
-let api: TmdbApi = new TmdbApi(tmdbApiKey || "");
 
 function PersonSearchField({
   name,
@@ -35,13 +26,14 @@ function PersonSearchField({
   const [fieldValue, setFieldValue] = useState("");
   const [results, setResults] = useState<StrictSearchResultProps[]>([]);
   const [showNoResults, setShowNoResults] = useState(false);
-  // const [selectedPersonId, setSelectedPersonId] = useState("");
+
+  const tmdbApi = getTmdbApiClient();
 
   const searchDebounced = useRef(
     debounce((searchValue) => {
       setIsLoading(true);
 
-      api.search.people(searchValue).subscribe((people) => {
+      tmdbApi.search.people(searchValue).subscribe((people) => {
         setResults(convertTmdbPeopleToSemanticSearchResults(people.results));
         setIsLoading(false);
         setShowNoResults(true);
@@ -61,7 +53,7 @@ function PersonSearchField({
             (movieOrShow as Movie)["title"] ?? (movieOrShow as TvShow)["name"]
         )
         .join(", "),
-      image: `https://image.tmdb.org/t/p/w300/${person.profile_path}`,
+      image: `${TMDB_BASE_URL}${person.profile_path}`,
     }));
   }
 
